5850ee2844b9b0626f1a562c3de2762794a5939f
hopegiver <hopegiver@gmail.com>
2019-04-29 00:23:09 +0900

    a

--- js/app.js
+++ js/app.js
@@ -1,39 +1,46 @@
-var Index = { template: '<url>pages/index.html</url>' }
-var Foo = { template: '<url>pages/foo.html</url>' }
-var Bar = { template: '<url>pages/bar.html</url>' }
-var About = { template: '<url>pages/about.html</url>' }
+var Index = { template: '<!---->', templateUrl: 'pages/index.html' }
+var Foo = { template: '<!---->', templateUrl: 'pages/foo.html' }
+var Bar = { template: '<!---->', templateUrl: 'pages/bar.html' }
+var About = { template: '<!---->', templateUrl: 'pages/about.html' }
 
 var router = new VueRouter({
-    routes: [
-        { path: '/', component: Index },
-        { path: '/foo', component: Foo },
-        { path: '/bar', component: Bar },
-        { path: '/about', component: About }
-    ]
+  routes: [
+    { path: '/', component: Index },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar },
+    { path: '/about', component: About }
+  ]
 });
 
 router.beforeEach((to, from, next) => {
-    var comp = to.matched[0].components.default;
-    if(comp.template.startsWith('<url>') && comp.template.endsWith('</url>')) {
-        var url = comp.template.replace(/<\/?[^>]+(>|$)/g, "");
-        call(url, function(str) {
-            comp.template = str;
-            next();            
-        });
-    }
+  var comp = to.matched[0].components.default;
+  if(comp.template == '<!---->' && comp.templateUrl != undefined) {
+    new Promise(function(resolve, reject) {
+      ajax_get(comp.templateUrl, function(data) {
+        resolve(data);
+      });
+    }).then(function(data) {
+      console.log(data);
+      comp.template = data;
+      next();      
+    });
+  } else {
+    next();
+  }
 });
 
 var app = new Vue({
   router
 }).$mount('#app');
 
-function call(url, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          callback(this.responseText);
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+function ajax_get(url, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this.responseText);
+    }
+  };
+  url += (url.indexOf("?") > 0 ? "&_=" : "?_=") + new Date().getTime();
+  xhttp.open("GET", url, true);
+  xhttp.send();
 }
